4b966035686fa29d861c3254cbba20af9959ff1c
Kevin Ng <kevinngsw@gmail.com>
2014-01-25 22:23:15 +0800

    Added symbolic constants.

--- chapter_1/fahrenheit_celsius_for.c
+++ chapter_1/fahrenheit_celsius_for.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define	LOWER	0 		/* lower limit of table */
+#define	UPPER	300		/* upper limit */
+#define	STEP	20		/* step size */
+
 int main(int argc, char const *argv[])
 {
 	int fahr;
 
 	printf("Fahrenheit to celsius table:\n");
-	for (fahr = 300; fahr > 0; fahr -= 20) {
+	for (fahr = UPPER; fahr > LOWER; fahr -= STEP) {
 		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 	}
 	return 0;
