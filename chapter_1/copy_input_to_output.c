e1c9daad004ef894740da2863368a33392306bfe
Kevin Ng <kevinngsw@gmail.com>
2014-01-25 22:50:12 +0800

    Added comments stating why we cannot use char for the datatype of c.

--- chapter_1/copy_input_to_output.c
+++ chapter_1/copy_input_to_output.c
@@ -3,7 +3,7 @@
 /* copy input to output; 1st version. */
 int main(int argc, char const *argv[])
 {
-	int c;
+	int c; /* we can't use char because the datatype needs to be big enough to hold any char value PLUS EOF. */
 
 	c = getchar();
 	while (c != EOF) {
