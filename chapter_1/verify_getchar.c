1036d890925f4658eecdc54ece584593f89fbabf
Kevin Ng <kevinngsw@gmail.com>
2014-01-27 10:29:13 +0800

    Fix.

--- chapter_1/verify_getchar.c
+++ chapter_1/verify_getchar.c
@@ -3,13 +3,11 @@
 int main(int argc, char const *argv[])
 {
 	int c;
-
+		
 	while ((c = getchar()) != EOF) {
-		if (c != '\n')
-		{
+		if (c != '\n') {
 			printf("Not EOF\n");
 		}
-	}
-	
+	}	
 	return 0;
 }
