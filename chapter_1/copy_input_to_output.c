6ae3608db8673e860ed0520b1760042fedd8cf93
Kevin Ng <kevinngsw@gmail.com>
2014-01-27 11:20:24 +0800

    Page 20, Exercise 1-9

--- chapter_1/copy_input_to_output.c
+++ chapter_1/copy_input_to_output.c
@@ -5,8 +5,17 @@ int main(int argc, char const *argv[])
 {
 	int c; /* we can't use char because the datatype needs to be big enough to hold any char value PLUS EOF. */
 
+	int last_c = 'a'; /* start with any non-blank character. */
 	while ((c = getchar()) != EOF) {
-		putchar(c);
+		if (c == ' ' && last_c == ' ')
+		{
+			/* do nothing. */
+		}
+		else
+		{
+			putchar(c);
+		}
+		last_c = c;
 	}
 	
 	return 0;
