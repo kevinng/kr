9a71a1b0cd278b80b214405023829d19f46fad87
Kevin Ng <kevinngsw@gmail.com>
2014-01-30 10:35:14 +0800

    Page 34, First Version of Fold

---
+++ chapter_1/fold.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define	MAX_WORD_WIDTH	25	
+#define	LINE_WIDTH	80
+
+int main(int argc, char const *argv[])
+{
+	char word[MAX_WORD_WIDTH];
+	
+	int c, i, j;
+	for (i = 0; (c = getchar()) != EOF; ++i)
+	{
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			word[j++] = c;
+			word[j] = '\0';
+			j = 0;
+
+			if (i > MAX_WORD_WIDTH)
+			{
+				putchar('\n');
+				i = 0;
+			}
+			
+			printf("%s\n", word);
+		}
+		else
+			word[j++] = c;
+	}
+	return 0;
+}
