8ddc72cd59e434d0de99078e6774ced6eacd7cc2
Kevin Ng <kevinngsw@gmail.com>
2014-01-30 12:00:13 +0800

    Page 34, Exercise 1-23

---
+++ chapter_1/remove_comments.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define	IN	1
+#define	OUT	0
+
+int main(int argc, char const *argv[])
+{
+	int state;
+	state = OUT;
+
+	int c, last_c;
+	last_c = '/';
+	while ((c = getchar()) != EOF)
+	{
+		if (last_c == '/' && c == '*')
+			state = IN;
+		else if (last_c == '*' && c == '/')
+			state = OUT;
+
+		if (state == OUT && c != '/' && last_c != '/')
+			putchar(last_c);
+		
+		last_c = c;
+	}
+	
+	return 0;
+}
